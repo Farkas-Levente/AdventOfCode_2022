c1ed6977e118c3ac0ad5fff75b860c2b1d64fff1
Levente Farkas <levente.farkas.256@gmail.com>
2023-11-10 07:50:47 +0100

    Day 2 Part 2 Complete

--- Day2/Day2.js
+++ Day2/Day2.js
@@ -11,15 +11,6 @@ const arr = syncReadFile("./Day2/Day2.txt");
 
 let myPoints = 0;
 
-arr.forEach((round) => {
-  calCulateInitialPoints(round);
-
-  calculateDraws(round);
-  calculateWins(round);
-});
-
-console.log(myPoints);
-
 function calculateWins(round) {
   if (round[0] === "A" && round[2] === "Y") {
     myPoints += 6;
@@ -47,3 +38,63 @@ function calCulateInitialPoints(round) {
     myPoints += 3;
   }
 }
+
+arr.forEach((round) => {
+  calCulateInitialPoints(round);
+
+  calculateDraws(round);
+  calculateWins(round);
+});
+
+// console.log(myPoints);
+
+//Part 2
+
+function calculateWinningPoints(opponent) {
+  let points = 0;
+  if (opponent === "A") {
+    points = 2;
+  } else if (opponent === "B") {
+    points = 3;
+  } else {
+    points = 1;
+  }
+  points += 6;
+  return points;
+}
+function calculateLosingPoints(opponent) {
+  let points = 0;
+  if (opponent === "A") {
+    points = 3;
+  } else if (opponent === "B") {
+    points = 1;
+  } else {
+    points = 2;
+  }
+  return points;
+}
+function calculateDrawPoints(opponent) {
+  let points = 0;
+  if (opponent === "A") {
+    points = 1;
+  } else if (opponent === "B") {
+    points = 2;
+  } else {
+    points = 3;
+  }
+  points += 3;
+  return points;
+}
+
+let secondRoundPoints = 0;
+
+arr.forEach((round) => {
+  if (round[2] === "X") {
+    secondRoundPoints += calculateLosingPoints(round[0]);
+  } else if (round[2] === "Y") {
+    secondRoundPoints += calculateDrawPoints(round[0]);
+  } else {
+    secondRoundPoints += calculateWinningPoints(round[0]);
+  }
+});
+console.log(secondRoundPoints);

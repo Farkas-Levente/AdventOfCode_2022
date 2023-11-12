5bacba76f42591c4a4661df551a3f248d6c4efa5
Levente Farkas <levente.farkas.256@gmail.com>
2023-11-12 14:39:46 +0100

    Day 5 Part 1 Complete

---
+++ Day5/Day5.js
@@ -0,0 +1,45 @@
+const { syncReadFile } = require("../FileRead.js");
+
+const arr = syncReadFile("./Day5/Day5.txt");
+
+//Read in the crate positions
+let stacks = {};
+arr.every((line) => {
+  let counter = 1;
+  for (i = 0; i < line.length; i += 4) {
+    const sliced = line.slice(i, i + 3);
+    if (sliced.startsWith("[")) {
+      stacks[counter] = stacks[counter] || []; // Initialize as an array if it doesn't exist
+      stacks[counter].push(sliced);
+    }
+    counter++;
+  }
+  //Execute until it reaches the 1 2 3 line
+  if (line[1] == 1) {
+    return false;
+  }
+  return true;
+});
+
+//Read in the commands
+let commands = [];
+arr.forEach((line) => {
+  if (line.startsWith("move")) {
+    const splits = line.split(" ");
+    commands.push({ amount: splits[1], from: splits[3], to: splits[5] });
+  }
+});
+
+//Move the Crates
+
+commands.forEach((command) => {
+  MoveCrate(command.amount, command.from, command.to);
+});
+
+function MoveCrate(amount, from, to) {
+  for (i = 0; i < amount; i++) {
+    const shifted = stacks[from].shift();
+    stacks[to].unshift(shifted);
+  }
+}
+console.log(stacks);
